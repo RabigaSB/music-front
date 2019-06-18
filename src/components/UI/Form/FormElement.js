212f905d862ffb42c2a8dd9ffb6132ba8b17bf5a
Rabiga Sarbassova <iamrabiga@gmail.com>
2019-06-18 19:21:45 +0600

    HW#89: Можно админу публиковать новый альбом

--- src/components/UI/Form/FormElement.js
+++ src/components/UI/Form/FormElement.js
@@ -4,6 +4,16 @@ import PropTypes from 'prop-types';
 
 
 const FormElement = props => {
+	let formControlChildren;
+
+	if(props.type === "select" && props.options) {
+		formControlChildren = props.options.map(option => {
+			return <option value={option._id} key={option._id}>{option.name}</option>});
+
+		formControlChildren.unshift(
+			<option value="" key={1}  disabled selected>Please select {props.name}...</option>);
+	}
+
 	return (
 		<FormGroup row>
 			<Label for='username'>{props.title}</Label>
@@ -16,8 +26,12 @@ const FormElement = props => {
 					onChange={props.onChange}
 					invalid={!!props.error}
 					required={props.required}
+					{...props}
 
-				/>
+				>
+					{formControlChildren}
+				</Input>
+
 				{props.error &&
 				(<FormFeedback>
 					{props.error}
@@ -38,7 +52,8 @@ FormElement.propTypes = {
 	type: PropTypes.string.isRequired,
 	required: PropTypes.bool,
 	placeholder: PropTypes.string,
-	value: PropTypes.string.isRequired,
+	value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 	onChange: PropTypes.func.isRequired,
 	error: PropTypes.string,
+	options: PropTypes.arrayOf(PropTypes.object)
 };
--- src/containers/Albums/NewAlbum.js
+++ src/containers/Albums/NewAlbum.js
@@ -1,26 +1,120 @@
 import React, {Component, Fragment} from 'react';
 
 import {connect} from "react-redux";
-import {createAlbum} from "../../store/actions/action-music";
+import {createAlbum, fetchArtists} from "../../store/actions/action-music";
+import {Button, Form} from 'reactstrap';
+import FormElement from '../../components/UI/Form/FormElement';
 
 
 class NewAlbum extends Component {
-	
+	state = {
+		name: '',
+		published: false,
+		year: '',
+		artist: '',
+	};
+
+	componentDidMount() {
+		this.props.onFetchArtists();
+	}
+
+	submitFormHandler = event => {
+		event.preventDefault();
+
+		const formData = new FormData();
+
+		for (let key in this.state) {
+			formData.append(key, this.state[key]);
+		}
+		this.props.onSubmit(formData);
+	};
+
+	inputChangeHandler = event => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
+
+	inputChangeCheckboxHandler = event => {
+		this.setState({
+			[event.target.name]: event.target.checked
+		});
+	};
+
+	fileChangeHandler = e => {
+		this.setState({image: e.target.files[0]})
+	};
+
 	render() {
 		return (
-				<Fragment>
-					here will be form
-				</Fragment>
+			<Fragment>
+				<Form onSubmit={this.submitFormHandler}>
+					<FormElement
+						title="Name"
+						type="text"
+						required
+						name="name"
+						placeholder="Enter name"
+						value={this.state.name}
+						onChange={this.inputChangeHandler}
+					/>
+					<FormElement
+						title="Year"
+						type="number"
+						required
+						min="0"
+						name="year"
+						placeholder="Enter year"
+						value={this.state.year}
+						onChange={this.inputChangeHandler}
+					/>
+					<FormElement
+						title="Published"
+						type="checkbox"
+						required
+						name="published"
+						checked={this.state.published}
+						onChange={this.inputChangeCheckboxHandler}
+					/>
+					<FormElement
+						title="Image"
+						type="file"
+						name="image"
+						placeholder="Enter image"
+						value={this.state.image}
+						onChange={this.fileChangeHandler}
+					/>
+					<FormElement
+						title="Artist"
+						type="select"
+						required
+						name="artist"
+						options={this.props.artists}
+						value={this.state.artist}
+						onChange={this.inputChangeHandler}
+					/>
+
+
+					<Button type="submit" color="primary" >Save</Button>
+
+				</Form>
+			</Fragment>
 		);
 	}
 }
 
+const mapStateToProps = state => {
+	return {
+		artists: state.music.artists
+	}
+};
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onPostAlbums: data => dispatch(createAlbum(data))
+		onSubmit: data => dispatch(createAlbum(data)),
+		onFetchArtists: () => dispatch(fetchArtists())
 	};
 };
 
-export default connect(null, mapDispatchToProps)(NewAlbum);
+export default connect(mapStateToProps, mapDispatchToProps)(NewAlbum);
 
